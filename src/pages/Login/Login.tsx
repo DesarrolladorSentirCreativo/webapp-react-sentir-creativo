8427d2cd728c0a8a0a2e634eec7de103d35fd7bb
Angel Sepulveda <angelsepulvedasep@gmail.com>
2023-03-19 14:36:49 -0300

    implementación de validaciones en login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,9 @@
 import './Login.css'
 
 import { Box, Button, Container, Grid, Paper, TextField, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import React from 'react'
+import * as yup from 'yup'
 
 interface LoginType {
   email: string
@@ -9,20 +11,28 @@ interface LoginType {
 }
 
 const Login: React.FC = () => {
-  const [loginData, setLoginData] = useState<LoginType>({
-    email: '',
-    password: ''
+  const formik = useFormik<LoginType>({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: yup.object().shape({
+      email: yup
+        .string().trim()
+        .email('El correo electrónico es invalido')
+        .required('El correo electrónico es obligatorio'),
+      password: yup
+        .string()
+        .trim()
+        .min(6, 'La contraseña debe tener al menos 6 caracteres')
+        .required('La contraseña es obligatoria')
+    }
+    ),
+    onSubmit: (value) => {
+      console.log('funciona')
+    }
+
   })
-
-  const handleChangeEventLogin = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = (e: React.FormEvent<HTMLInputElement>): void => {
-    e.preventDefault()
-    console.log(loginData)
-  }
-
   return (
         <Container maxWidth='sm'>
             <Grid container direction='column' alignItems='center' justifyContent='center' sx={{ minHeight: '90vh' }}>
@@ -34,11 +44,18 @@ const Login: React.FC = () => {
                             className='logo'
                         />
                         <Typography variant='h5'>Inicio de Sesión</Typography>
-                        <Box component='form' onSubmit={handleSubmit}>
-                            <TextField type='email' name='email' margin='normal' fullWidth label='correo electrónico'
-                                       sx={{ mt: 2, mb: 1.5 }} required onChange={handleChangeEventLogin}/>
+                        <Box component='form' onSubmit={formik.handleSubmit}>
+                            <TextField name='email' margin='normal' fullWidth label='correo electrónico'
+                                       sx={{ mt: 2, mb: 1.5 }} required value={formik.values.email}
+                                       onChange={formik.handleChange}
+                                       error={(formik.touched.email === true) && Boolean(formik.errors.email)}
+                                       helperText={(formik.touched.email === true) && formik.errors.email} />
                             <TextField name='password' margin='normal' type='password' fullWidth label='contraseña'
-                                       sx={{ mt: 2, mb: 1.5 }} required onChange={handleChangeEventLogin}/>
+                                       sx={{ mt: 2, mb: 1.5 }} required
+                                       value={formik.values.password}
+                                       onChange={formik.handleChange}
+                                       error={(formik.touched.password === true) && Boolean(formik.errors.password)}
+                                       helperText={(formik.touched.password === true) && formik.errors.password} />
                             <Button fullWidth type='submit' variant='contained'>Iniciar Sesión</Button>
                         </Box>
                         <Typography variant='body2' sx={{ p: 5, color: '#757575' }}>Copyright © Sentir Creativo
