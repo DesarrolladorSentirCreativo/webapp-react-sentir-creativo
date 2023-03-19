47db75480de7b08699ca85502bd462a1a3ec921e
Angel Sepulveda <angelsepulvedasep@gmail.com>
2023-03-19 14:03:46 -0300

    implementación de manejo de estado en login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,30 +1,52 @@
 import './Login.css'
 
 import { Box, Button, Container, Grid, Paper, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+
+interface LoginType {
+  email: string
+  password: string
+}
 
 const Login: React.FC = () => {
+  const [loginData, setLoginData] = useState<LoginType>({
+    email: '',
+    password: ''
+  })
+
+  const handleChangeEventLogin = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLInputElement>): void => {
+    e.preventDefault()
+    console.log(loginData)
+  }
+
   return (
-      <Container maxWidth="sm">
-        <Grid container direction="column" alignItems="center" justifyContent="center" sx={{ minHeight: '90vh' }}>
-            <Grid item>
-                <Paper sx={{ padding: '1.2em' }}>
-                  <img
-                      src="/static/images/full-logo.png"
-                      alt="SentirCreativo"
-                      className="logo"
-                  />
-                    <Typography variant="h5">Inicio de Sesión</Typography>
-                    <Box component="form">
-                          <TextField margin="normal" fullWidth label="correo electrónico" sx={{ mt: 2, mb: 1.5 }}/>
-                          <TextField margin="normal" type="password" fullWidth label="contraseña" sx={{ mt: 2, mb: 1.5 }}/>
-                      <Button fullWidth type="submit" variant="contained">Iniciar Sesión</Button>
-                    </Box>
-                  <Typography variant="body2" sx={{ p: 5, color: '#757575' }}>Copyright © Sentir Creativo 2023</Typography>
-                </Paper>
+        <Container maxWidth='sm'>
+            <Grid container direction='column' alignItems='center' justifyContent='center' sx={{ minHeight: '90vh' }}>
+                <Grid item>
+                    <Paper sx={{ padding: '1.2em' }}>
+                        <img
+                            src='/static/images/full-logo.png'
+                            alt='SentirCreativo'
+                            className='logo'
+                        />
+                        <Typography variant='h5'>Inicio de Sesión</Typography>
+                        <Box component='form' onSubmit={handleSubmit}>
+                            <TextField type='email' name='email' margin='normal' fullWidth label='correo electrónico'
+                                       sx={{ mt: 2, mb: 1.5 }} required onChange={handleChangeEventLogin}/>
+                            <TextField name='password' margin='normal' type='password' fullWidth label='contraseña'
+                                       sx={{ mt: 2, mb: 1.5 }} required onChange={handleChangeEventLogin}/>
+                            <Button fullWidth type='submit' variant='contained'>Iniciar Sesión</Button>
+                        </Box>
+                        <Typography variant='body2' sx={{ p: 5, color: '#757575' }}>Copyright © Sentir Creativo
+                            2023</Typography>
+                    </Paper>
+                </Grid>
             </Grid>
-        </Grid>
-      </Container>
+        </Container>
   )
 }
 
