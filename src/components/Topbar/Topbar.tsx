ae4a1b1dcdefc359e2d45d114a407f295de1b884
Angel Sepulveda <angelsepulvedasep@gmail.com>
2023-05-04 18:44:48 -0400

    mejora en obtencion nombre de usuario

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,17 +1,40 @@
 import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, Avatar, Box, Grid, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@mui/material'
+import { Width } from 'devextreme-react/chart'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { themePallete } from '../../config/config.theme'
 import sizeConfig from '../../config/size.config.'
+import { getLocalStorage } from '../../helpers/localstorage.helper'
 import authService from '../../services/auth.service'
 interface Props {
   handleDrawerToggle: () => void
 }
+
+interface User {
+  userId: number
+  email: string
+  userName: string
+}
+
 const Topbar: React.FC<Props> = (props) => {
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
-  const [user, setUser] = useState<string | null>(null)
+  const [user, setUser] = useState<User>({
+    userId: 0,
+    email: '',
+    userName: ''
+  })
   const navigate = useNavigate()
 
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>): void => {
@@ -23,9 +46,11 @@ const Topbar: React.FC<Props> = (props) => {
   }
 
   useEffect(() => {
-    const userData = localStorage.getItem('user')
-    if (userData !== null) {
+    const userData = getLocalStorage('user') || '{}'
+    try {
       setUser(JSON.parse(userData))
+    } catch (error) {
+      console.error('Error parsing user data:', error)
     }
   }, [])
 
@@ -48,79 +73,103 @@ const Topbar: React.FC<Props> = (props) => {
   ]
 
   return (
-        <AppBar position='fixed'
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { md: `calc(100% - ${sizeConfig.sidebar.width})` },
+        ml: sizeConfig.sidebar.width,
+        boxShadow: 'unset',
+        backgroundColor: themePallete.PRIMARY,
+        color: themePallete.CONTRAST_TEXT
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={props.handleDrawerToggle}
+          sx={{ mr: 2, display: { md: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box sx={{ flexGrow: 0, marginLeft: 'auto' }}>
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item xs={6} sx={{ marginBottom: 2 }}>
+              <IconButton
+                onClick={handleOpenUserMenu}
                 sx={{
-                  width: { md: `calc(100% - ${sizeConfig.sidebar.width})` },
-                  ml: sizeConfig.sidebar.width,
-                  boxShadow: 'unset',
-                  backgroundColor: themePallete.PRIMARY,
-                  color: themePallete.CONTRAST_TEXT
-                }}>
-            <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    edge="start"
-                    onClick={props.handleDrawerToggle}
-                    sx={{ mr: 2, display: { md: 'none' } }}
+                  p: 0,
+                  mr: 10,
+                  display: 'flex',
+                  alignContent: 'center',
+                  alignItems: 'center',
+                  mt: 2,
+                  width: '100%',
+                  textTransform: 'none'
+                }}
+              >
+                <Avatar
+                  alt="Remy Sharp"
+                  src={'/static/images/user.jpg'}
+                  sx={{ mr: 2, height: '25px', width: '25px' }}
+                />
+                <Typography
+                  variant="button"
+                  display="block"
+                  gutterBottom
+                  sx={{
+                    color: '#fff',
+                    fontSize: '12px',
+                    width: '100%',
+                    textTransform: 'none'
+                  }}
                 >
-                    <MenuIcon />
-                </IconButton>
-                <Box sx={{ flexGrow: 0, marginLeft: 'auto' }}>
-                    <Grid
-                        container
-                        spacing={0}
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="center"
+                  {user.userName}
+                </Typography>
+              </IconButton>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting.key}
+                    onClick={setting.onClick}
+                    sx={{ width: '150px' }}
+                  >
+                    <Typography
+                      textAlign="center"
+                      sx={{ fontSize: 15, width: '150px' }}
                     >
-                        <Grid item xs={6} sx={{ marginBottom: 2 }}>
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, mr: 10, display: 'flex', alignContent: 'center', alignItems: 'center', mt: 2 }}>
-                                <Avatar
-                                    alt="Remy Sharp"
-                                    src={
-                                        '/static/images/user.jpg'
-                                    }
-                                    sx={{ mr: 2, height: '25px', width: '25px' }}
-                                />
-                                <Typography
-                                    variant="button"
-                                    display="block"
-                                    gutterBottom
-                                    sx={{ color: '#fff', fontSize: '10px' }}
-                                >
-                                    hOLA MUNDO
-                                </Typography>
-                            </IconButton>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                  vertical: 'top',
-                                  horizontal: 'right'
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                  vertical: 'top',
-                                  horizontal: 'right'
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting.key} onClick={setting.onClick} sx={{ width: '150px' }}>
-                                        <Typography textAlign="center" sx={{ fontSize: 15, width: '150px' }}>
-                                            {setting.text}
-                                        </Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Toolbar>
-        </AppBar>
+                      {setting.text}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Grid>
+          </Grid>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+import { setLocalStorage } from '../helpers/localstorage.helper'
 import { type LoginData } from '../models'
 
 const login = async (values: LoginData): Promise<void> => {
   const { data } = await axios.post('/userAdmin', values)
-  localStorage.setItem('token', data.token)
+  setLocalStorage('token', data.token)
+  setLocalStorage('user', JSON.stringify(data.user))
 }
 
 export default { login }
